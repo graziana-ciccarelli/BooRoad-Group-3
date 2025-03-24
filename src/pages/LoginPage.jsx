15dcea9dc0adf6424347d7a0728b76b1dffb96a9
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-03-24 11:19:42 +0100

    add link icon

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,26 +1,11 @@
+import Form from "../components/Form"
 export default function LoginPage() {
     return(
         <>
         <header class="header">
         <h1><span class="brand-name">Boo</span>Road</h1>
         </header>
-        
-        <div className="login-container">
-            <h2>Log in to BooRoad</h2>
-            <form id="loginForm" onsubmit="return validateForm(event)">
-                <label for="username">Nome Utente:</label>
-                <div className="input-container">
-                    <i className="fas fa-user"></i>
-                    <input type="text" id="username" name="username" required placeholder="Nome utente"/>
-                </div>
-                <label for="password">Password:</label>
-                <div className="input-container">
-                    <i className="fas fa-lock"></i>
-                    <input type="password" id="password" name="password" required placeholder="Password" minlength="6"/>
-                </div>
-                <button type="submit">Accedi</button>
-            </form>
-        </div>
+        <Form/>
         </>
     )
 }
