d8074bb2421efd28cde6d31f845fd534480230be
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-03-24 10:33:04 +0100

    add: start

---
+++ src/layout/DefaultLayout.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
+
+export default function DefaultLayout() {
+  return (
+    <>
+     
+      <Header />
+      
+     
+      <main className="container">
+        <Outlet />
+      </main>
+    </>
+  );
+}
