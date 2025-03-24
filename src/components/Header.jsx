50a3b04af6cecf5b35ca10ce41d2741b28c196db
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-03-24 11:23:20 +0100

    add componet header

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default Header(){
+    return (
+        <header class="header">
+            <h1><span class="brand-name">Boo</span>Road</h1>
+        </header>
+    )
+}
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,9 +2,7 @@ import Form from "../components/Form"
 export default function LoginPage() {
     return(
         <>
-        <header class="header">
-        <h1><span class="brand-name">Boo</span>Road</h1>
-        </header>
+        
         <Form/>
         </>
     )
